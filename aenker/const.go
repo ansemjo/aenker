35dbeae43ea2487756c9a61b6ee19832b9968517
Anton Semjonov <anton@semjonov.de>
2018-08-10 18:00:40 +0200

    increase chunksize to 16kB

--- aenker/const.go
+++ aenker/const.go
@@ -8,7 +8,7 @@ const (
 	KeyLength = 32
 
 	// ChunkSize is the amount of plaintext that is encrypted per chunk
-	ChunkSize = 256
+	ChunkSize = 16 * 1024
 
 	// ErrExtraData indicates that there is extra data appended to the ciphertext
 	ErrExtraData = ce.ConstError("aenker: extraneous data after ciphertext")
