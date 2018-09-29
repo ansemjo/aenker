f765e603ff9cb1808a2ecbb100a6843ee72f73fb
Anton Semjonov <anton@semjonov.de>
2018-09-30 01:44:32 +0200

    added key derivation func for upcoming v2 format

---
+++ Aenker/key_derivation.go
@@ -0,0 +1,34 @@
+package aenker
+
+import (
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/hkdf"
+	"hash"
+	"io"
+)
+
+func KDF(userkey, salt []byte, info string) (key []byte) {
+
+	// lambda function to conform to hkdf's hash type
+	blake := func() hash.Hash {
+		h, err := blake2b.New384(nil)
+		if err != nil {
+			// since we don't use a key, this shouldn't happen
+			panic(err)
+		}
+		return h
+	}
+
+	hkdf := hkdf.New(blake, key, salt, []byte(info))
+
+	key = make([]byte, 32)
+	_, err := io.ReadFull(hkdf, key)
+	if err != nil {
+		// we only need 32 bytes .. i'm confident that there
+		// should be no error
+		panic(err)
+	}
+
+	return
+
+}
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/russross/blackfriday v1.5.1 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.2 // indirect
-	golang.org/x/crypto v0.0.0-20180808211826-de0752318171
+	golang.org/x/crypto v0.0.0-20180927165925-5295e8364332
 	golang.org/x/sys v0.0.0-20180810173357-98c5dad5d1a0 // indirect
 	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
